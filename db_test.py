3a193d577c79f6ca49e230ded3a78c6220763133
Bryan Orozco <Hoodini7531@gmail.com>
2025-09-04 20:40:50 -0700

    created scaffold project structure (db, models, repositories, scripts)

--- db_test.py
+++ db_test.py
@@ -18,8 +18,25 @@ cursor = conn.cursor()
 # run query
 cursor.execute("show tables;")
 results = cursor.fetchall()
-
 print(results)
+
+#read from data 
+cursor.execute("select id, username FROM users;")
+rows = cursor.fetchall()
+
+for row in rows:
+    print (row)
+
+#add data 
+cursor.execute("insert into users (username) values (%s)", ("maria",))
+conn.commit()
+cursor.execute("select * from users;")
+new_users = cursor.fetchall()
+print (new_users)
+
+
+
+
 
 #close the door/ exit database 
 cursor.close()
